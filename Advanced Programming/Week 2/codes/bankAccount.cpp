1841a27f7467209c7528776e6e155c9a5fa21a4f
Khthonian <anansicorp@tuta.io>
2023-10-01 18:06:06 +0100

    feat(adv-prog): Add bankAccount.cpp

---
+++ Advanced Programming/Week 2/codes/bankAccount.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class BankAccount {
+    // Define class properties and methods here
+    // Remember to include constructors and access specifiers (public/private)
+};
+
+int main() {
+    // Create instances of the BankAccount class and demonstrate its methods
+    // Example:
+    // BankAccount account1("12345", "John Doe", 1000.0);
+    // account1.Deposit(500);
+    // account1.Withdraw(200);
+    // account1.DisplayAccountInfo();
+
+    return 0;
+}
