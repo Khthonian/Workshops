015156590b615aecc5b9b527101e814dac069436
Khthonian <anansicorp@tuta.io>
2023-10-01 18:45:57 +0100

    feat(adv-prog): Initialise bank account
    The bank account class is initialised according to the instructions in
    the brief, prompting the user for inputs rather than taking them as
    arguments. Ideally, the account number would be randomly generated but
    the brief doesn't require that.
    
    Outputs for financial values are formatted such that they look like
    normal monetary values e.g. $140.20.

--- Advanced Programming/Week 2/codes/bankAccount.cpp
+++ Advanced Programming/Week 2/codes/bankAccount.cpp
@@ -1,18 +1,59 @@
+#include <iomanip>
 #include <iostream>
+#include <ostream>
 #include <string>
 
 class BankAccount {
-    // Define class properties and methods here
-    // Remember to include constructors and access specifiers (public/private)
+	public:
+		// Constructor
+		BankAccount() {
+			// Prompt the user for an account number
+			std::cout << "Enter an account number: ";
+			std::cin >> accountNumber;
+
+			// Prompt the user for a name for the account
+			std::cout << "Enter an account holder name: ";
+			std::cin >> accountName;
+		
+			// Prompt the user for a starting balance
+			std::cout << "Enter an initial balance: ";
+			std::cin >> balance;
+		}
+		
+		// Declare a function to deposit to the balance
+		void Deposit(float x) {
+			balance += x;
+			std::cout << std::endl;
+			std::cout << "Deposited $" << std::fixed << std::setprecision(2) << x << " into account." << std::endl;
+		}
+		
+		// Declare a function to withdraw from the balance
+		void Withdraw(float x) {
+			balance -= x;
+			std::cout << std::endl;
+			std::cout << "Withdrawn $" << std::fixed << std::setprecision(2) << x << " into account." << std::endl;
+		}
+
+		// Declare a function to output the account information
+		void DisplayAccountInfo() {
+			std::cout << std::endl;
+			std::cout << "Account Number: " << accountNumber << std::endl;
+			std::cout << "Account Holder: " << accountName << std::endl;
+			std::cout << "Balance: $" << std::fixed << std::setprecision(2) << balance << std::endl;
+		}
+
+	private:
+		// Privately declare the account variables
+		std::string accountNumber;
+		std::string accountName;
+		float balance;
 };
 
 int main() {
-    // Create instances of the BankAccount class and demonstrate its methods
-    // Example:
-    // BankAccount account1("12345", "John Doe", 1000.0);
-    // account1.Deposit(500);
-    // account1.Withdraw(200);
-    // account1.DisplayAccountInfo();
+    BankAccount account1;
+    account1.Deposit(500);
+    account1.Withdraw(200);
+    account1.DisplayAccountInfo();
 
     return 0;
 }
