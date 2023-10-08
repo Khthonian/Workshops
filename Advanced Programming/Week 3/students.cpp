34e9449c4f2afd0c27ab5c23e427e3f9b0efce30
Khthonian <anansicorp@tuta.io>
2023-10-08 18:45:38 +0100

    feat(adv-prog): Add Student class

---
+++ Advanced Programming/Week 3/students.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Student {
+	public:
+		// Constructor
+		Student(int newScore) {
+			// Assign the user input to the class variable
+			score = newScore;
+		}
+
+	private:
+		// Declare the score variable
+		int score;
+};
+
+int main (int argc, char *argv[]) {
+	
+
+
+	return 0;
+}
