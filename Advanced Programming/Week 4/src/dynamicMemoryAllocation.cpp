2754cd4e9315c753811b36dcd0f5af510386ed2e
Khthonian <anansicorp@tuta.io>
2023-10-16 11:46:52 +0100

    feat(adv-prog): Complete Task 1 Question 2

---
+++ Advanced Programming/Week 4/src/dynamicMemoryAllocation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <ostream>
+
+int main() {
+	// Declare an integer pointer
+	int* dynamicInt;
+
+	// Dynamically allocate memory for an integer
+	dynamicInt = new int;
+
+	// Validate the memory allocation
+	if (dynamicInt == nullptr) {
+		std::cerr << "Failed memory allocation!" << std::endl;
+		return 1;
+	}
+
+	// Prompt the user for an integer input
+	std::cout << "Enter an integer: ";
+	int userInput;
+	std::cin >> userInput;
+
+	// Store the user input in the allocated memory
+	*dynamicInt = userInput;
+
+	// Print the stored value
+	std::cout << "Stored value: " << *dynamicInt << std::endl;
+
+	// Deallocate the memory
+	delete dynamicInt;
+
+	return 0;
+}
