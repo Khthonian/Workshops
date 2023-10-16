e58f1b94b626d563f87711a1e90ee0f8cd746d15
Khthonian <anansicorp@tuta.io>
2023-10-16 12:52:47 +0100

    feat(adv-prog): Complete Task 1 Question 8

---
+++ Advanced Programming/Week 4/src/pointerArithmetic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+	// Declare an integer array
+	int arr1[5] = {10, 20, 30, 40, 50};
+
+	// Declare an integer pointer to the third element of the array
+	int* intPtr = &arr1[2];
+
+	// Select and print the second element
+	intPtr--;
+	std::cout << *intPtr << std::endl;
+
+	// Select and print the fourth element
+	intPtr += 2;
+	std::cout << *intPtr << std::endl;
+
+	return 0;
+}
