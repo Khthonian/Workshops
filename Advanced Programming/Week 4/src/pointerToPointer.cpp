e278eb027fd9fc9dea0136a139a7332896c6e53b
Khthonian <anansicorp@tuta.io>
2023-10-16 12:06:33 +0100

    feat(adv-prog): Complete Task 1 Question 4

---
+++ Advanced Programming/Week 4/src/pointerToPointer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	// Declare an integer variable
+	int num1 = 100;
+
+	// Declare a pointer to the address of num1
+	int* ptr1 = &num1;
+
+	// Declare a pointer to the address of ptr1
+	int** ptr2 = &ptr1;
+
+	// Print the value of num1 before modification
+	std::cout << num1 << std::endl;
+
+	// Modify ptr2
+	**ptr2 = 200;
+
+	// Print the value of num1 after modification
+	std::cout << num1 << std::endl;
+
+	return 0;
+}
