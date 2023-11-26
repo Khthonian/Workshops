247a5442b0473f430e36dd17678f91d98d4a2df4
Khthonian <anansicorp@tuta.io>
2023-11-26 14:40:38 +0000

    feat(adv-prog): Add user input
    The user input has been added to the script. Extra input validation can
    be included but it does not seems necessary to satisfy the scope of the
    exercise.

--- Advanced Programming/Week 10/universitySystem.cpp
+++ Advanced Programming/Week 10/universitySystem.cpp
@@ -1,13 +1,16 @@
+// Copyright 2023 Stewart Charles Fisher II
+
+// Import libraries
 #include <iostream>
-#include <iterator>
+#include <limits>
 #include <string>
 
 class Person {
-protected:
+ protected:
   std::string name;
   int age;
 
-public:
+ public:
   Person(const std::string &n, int a) : name(n), age(a) {}
 
   std::string getName() const { return name; }
@@ -24,22 +27,22 @@ public:
 };
 
 class Student : public Person {
-private:
+ private:
   // Add private attributes for Student class
-  std::string studentID;
-  double gpa;
+  std::string _studentID_;
+  double _gpa_;
 
-public:
+ public:
   Student(const std::string &n, int a, const std::string &s, double g)
-      : Person(n, a), studentID(s), gpa(g) {}
+      : Person(n, a), _studentID_(s), _gpa_(g) {}
 
   // Getters for the attributes
-  std::string getID() const { return studentID; }
-  double getGpa() const { return gpa; }
+  std::string getID() const { return _studentID_; }
+  double getGpa() const { return _gpa_; }
 
   // Setters for the attributes
-  void setID(const std::string &s) { studentID = s; }
-  void setGpa(double g) { gpa = g; }
+  void setID(const std::string &s) { _studentID_ = s; }
+  void setGpa(double g) { _gpa_ = g; }
 
   void displayDetails() const override {
     std::cout << "Student Name: " << getName() << std::endl
@@ -51,23 +54,23 @@ public:
 };
 
 class Faculty : public Person {
-private:
+ private:
   // Add private attributes for Faculty class
-  std::string employeeID;
-  std::string department;
+  std::string _employeeID_;
+  std::string _department_;
 
-public:
+ public:
   Faculty(const std::string &n, int a, const std::string &e,
           const std::string &d)
-      : Person(n, a), employeeID(e), department(d) {}
+      : Person(n, a), _employeeID_(e), _department_(d) {}
 
   // Getters for the attributes
-  std::string getEmployeeID() const { return employeeID; }
-  std::string getDepartment() const { return department; }
+  std::string getEmployeeID() const { return _employeeID_; }
+  std::string getDepartment() const { return _department_; }
 
   // Setters for the attributes
-  void setEmployeeID(const std::string &e) { employeeID = e; }
-  void setDepartment(const std::string &d) { department = d; }
+  void setEmployeeID(const std::string &e) { _employeeID_ = e; }
+  void setDepartment(const std::string &d) { _department_ = d; }
 
   void displayDetails() const override {
     std::cout << "Faculty Name: " << getName() << std::endl
@@ -79,19 +82,19 @@ public:
 };
 
 class Staff : public Person {
-private:
+ private:
   // Add private attributes for Staff class
-  std::string staffID;
+  std::string _staffID_;
 
-public:
+ public:
   Staff(const std::string &n, int a, const std::string &s)
-      : Person(n, a), staffID(s) {}
+      : Person(n, a), _staffID_(s) {}
 
   // Getters for the attributes
-  std::string getStaffID() const { return staffID; }
+  std::string getStaffID() const { return _staffID_; }
 
   // Setters for the attributes
-  void setStaffID(const std::string &s) { staffID = s; }
+  void setStaffID(const std::string &s) { _staffID_ = s; }
 
   void displayDetails() const override {
     std::cout << "Staff Name: " << getName() << std::endl
@@ -102,13 +105,55 @@ public:
 };
 
 int main() {
+  // Dynamically declare the array and size
   const int SIZE = 5;
   Person *people[SIZE] = {nullptr};
 
-  // Create objects of different classes dynamically
-  people[0] = new Student("John Doe", 20, "S12345", 3.8);
-  people[1] = new Faculty("Dr. Smith", 40, "F98765", "Computer Science");
-  people[2] = new Staff("Jane Johnson", 30, "E54321");
+  // Declare variables to hold the inputs
+  std::string name;
+  int age;
+  double gpa;
+  std::string ID;
+  std::string department;
+  std::string personType;
+
+  // Loop through the array and input details
+  for (int i = 0; i < SIZE; ++i) {
+    std::cout << "Enter the details of Person " << i + 1 << ":" << std::endl;
+    std::cout << "Name: ";
+    std::getline(std::cin >> std::ws, name);
+    std::cout << "Age: ";
+    std::cin >> age;
+
+    std::cout << "Enter the type of person (S for Student, F for Faculty, ST "
+                 "for Staff): ";
+    std::cin >> personType;
+
+    if (personType == "S") {
+      std::cout << "Enter the Student ID: ";
+      std::cin >> ID;
+      std::cout << "Enter the GPA: ";
+      std::cin >> gpa;
+      people[i] = new Student(name, age, ID, gpa);
+    } else if (personType == "F") {
+      std::cout << "Enter the Faculty ID: ";
+      std::cin >> ID;
+      std::cout << "Enter the Department: ";
+      std::cin >> department;
+      people[i] = new Faculty(name, age, ID, department);
+    } else if (personType == "ST") {
+      std::cout << "Enter the Staff ID: ";
+      std::cin >> ID;
+      people[i] = new Staff(name, age, ID);
+    } else {
+      std::cout << "This is not a valid person type. Skipping to next person."
+                << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
 
   // Display details of each person using polymorphism
   for (int i = 0; i < SIZE; ++i) {
